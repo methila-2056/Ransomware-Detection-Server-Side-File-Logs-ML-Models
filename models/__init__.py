795758f83b2fb00b1ad930eb074034dcf0add726
methila-2056 <methilashiv@gmail.com>
2026-08-26 14:20:56 +0530

    feat: add models package init with model directory path

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+"""
+Ransomware Detection - ML Models Package
+
+This package contains trained model files (.joblib) and
+supports loading them via the MLEngine class.
+"""
+import os
+
+MODEL_DIR = os.path.dirname(__file__)
